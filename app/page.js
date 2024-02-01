67fd9f2f84a1ba89a75f086420d7219fcced0c60
prems09 <113038259+prems09@users.noreply.github.com>
2024-02-01 13:02:32 +0530

    Lets complete your tasks

---
+++ app/page.js
@@ -0,0 +1,75 @@
+"use client"
+import React, { useState } from 'react';
+
+const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [maintask, setMainTask] = useState([])
+  
+  const submitHander = (e)=>{
+    e.preventDefault()
+    setMainTask([...maintask,{title, desc}]);
+    settitle("")
+    setdesc("")
+    console.log(maintask)
+    }
+
+    const deleteHandler = (i) =>{
+      let copytask = [...maintask]
+      copytask.splice(i,1)
+      setMainTask(copytask)
+    }
+
+    let renderTask = <h2>No Task Available</h2>
+
+   if (maintask.length>0) {
+    renderTask = maintask.map((t,i)=>{
+      return (
+        <li key={i} className='flex items-center justify-between mb-5'>
+          <div className='flex items-center justify-between mb-5 w-2/3'>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-medium'>{t.desc}</h6>
+        </div> 
+        <button 
+        onClick={()=>{
+          deleteHandler(i)
+        }}
+        className='bg-red-400 px-4 py-2 text-white font-bold'>Delete</button>
+        </li>
+      )
+    })
+   }
+  return (
+   <>
+   <h1 className='bg-black text-white p-5 text-5xl font-bold text-center uppercase'>My Daily Tasks</h1>
+   <form action="" onSubmit={submitHander} className='mt-5 flex p-5 justify-center'>
+    <input
+     type="text" className='text-2xl border-zinc-900 border-4 px-4 py-2 m-2' 
+     placeholder='Enter Title here'
+     value={title}
+     onChange={(e)=>{
+      settitle(e.target.value)
+     }}
+    />
+    <input
+     type="text" className='text-2xl border-zinc-900 border-4 px-4 py-2 m-2' 
+     placeholder='Enter Description here'
+     value={desc}
+     onChange={(e)=>{
+      setdesc(e.target.value)
+     }}
+    />
+   <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded'>Add Task</button>
+   </form>
+
+   <hr />
+   <div className='p-9 bg-slate-300 '>
+     <ul>
+      {renderTask}
+     </ul>
+   </div>
+   </>
+  )
+}
+
+export default page
